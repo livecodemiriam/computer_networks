5294540f379021cad9018accd0cb811d458ad90b
penguinsheaven <miriamadriana.costan@gmail.com>
2018-05-22 06:59:39 +0300

    Homework2

---
+++ Tema2/ARP.py
@@ -0,0 +1,11 @@
+from scapy.all import *
+
+eth = Ether(dst = "ff:ff:ff:ff:ff:ff")
+arp = ARP(pdst = "198.13.13.0/16")
+answered, unanswered = srp(eth / arp, timeout = 10)
+checker = True 
+for answer in answered:
+    print answer[1].psrc, " - ", answer[1].hwsrc
+    if checker:
+        print answer[1].pdst, " - ", answer[1].hwdst
+        checker = False
